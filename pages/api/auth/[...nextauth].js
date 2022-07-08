256702f4a47d1e359433dab3c1ba0d76fae5081c
Satinworm <satinworm@gmail.com>
2022-07-08 15:09:47 +0300

    last try

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,8 +7,10 @@ export default NextAuth({
          clientId: process.env.GOOGLE_CLIENT_ID,
          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       }),
+
       // ...add more providers here
    ],
+   secret: process.env.SECRET,
    pages: {
       signIn: "/auth/signin",
    },
